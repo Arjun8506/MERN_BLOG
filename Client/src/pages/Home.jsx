51b362cf054c23e915725e6efed9c120df48aebd
Arjun8506 <140004161+Arjun8506@users.noreply.github.com>
2024-01-31 15:50:49 +0530

    home page created

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import '../styles/Home.css'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <h1>Welcome to my Blog</h1>
+      <p>Here you will find so many blogs related to <span>web developments</span> and so may answers of your questions too..</p>
+      <Link to={'/'} className='link' id='blogslink'>View all blogs here</Link>
+    </div>
   )
 }
 
